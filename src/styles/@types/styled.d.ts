f23cdd64d081fd64614473034eaa48667bd43f81
Guilherme Aquino <guiiaquino92@gmail.com>
2022-09-16 14:35:33 -0300

    add dist

--- src/styles/@types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components'
-import { defaultTheme } from '../themes/default'
-
-type ThemeType = typeof defaultTheme
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
-}
